c3f65ef9d43e77de6bd3a9f1216228dadbc54241
THANHPM <minhthanh95ptit@gmail.com>
2020-04-23 20:29:05 +0700

    03_Queryparameters

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 
 var app = express();
+var port = 3000;
 
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-var port = 3000;
+var users = [
+    {id: 1,name: 'Pham Minh Thanh'},
+    {id: 2,name: 'Pham Minh Thu'},
+];
 
 app.get('/', function(req, res){
     res.render('index',{
@@ -16,12 +20,21 @@ app.get('/', function(req, res){
 
 app.get('/users', function(req, res){
     res.render('users/index',{
-        users: [
-            {id: 1,name: 'Pham Minh Thanh'},
-            {id: 2,name: 'Pham Minh Thu'},
-        ]
+        users: users
     });
 })
+
+app.get('/users/search', function(req, res){
+    var q = req.query.q;
+
+    var matchUsers = users.filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;    
+    });
+
+    res.render('users/index',{
+        users: matchUsers
+    });
+});
 
 app.listen(port,function(){
     console.log('Listening in port' + port);
