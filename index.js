d4aab5c7085c8bfdf3c81d21ef5718a092b1debf
THANHPM <minhthanh95ptit@gmail.com>
2020-04-24 19:43:08 +0700

    07_View user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var low = require('lowdb');
+var shortid = require('shortid');
 var app = express();
 
 
@@ -50,9 +51,27 @@ app.get('/users/create', function(req, res){
     res.render('users/create');
 });
 
+app.get('/users/:id', function(req, res){
+  //  var id = parseInt(req.params.id); //parseInt khi so sanh id voi so
+     var id = req.params.id;
+     console.log(req);
+ //   console.log(typeof id);
+    var user = db.get('users').find({ id: id}).value();
+    
+    console.log(user);
+    res.render('users/view',{
+        user: user
+    })
+})
+
+
 app.post('/users/create', function(req, res){
     console.log(req.body);
-    db.get('users').value().push(req.body);
+    req.body.id = shortid.generate();
+    db.get('users')//Khong can .value() khi post
+    .push(req.body)
+    .write()
+    
     //create xong thi chuyen huong sang trang Users
     res.redirect('/users');
 });
