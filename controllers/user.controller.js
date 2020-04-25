5b8c383fcfb1d5bd68456141017645ee3bbd89f0
THANHPM <minhthanh95ptit@gmail.com>
2020-04-25 21:39:28 +0700

    MVC Controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,49 @@
+var db = require('../db');
+var shortid = require('shortid');
+
+module.exports.index = function(req, res){
+    res.render('users/index',{
+        users: db.get('users').value()
+    });
+};
+
+
+module.exports.search = function(req, res){
+    var q = req.query.q;
+
+    var matchUsers = db.get('users').value().filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;    
+    });
+
+    res.render('users/index',{
+        users: matchUsers
+    });
+};
+
+module.exports.create = function(req, res){
+    res.render('users/create');
+};
+
+module.exports.get = function(req, res){
+    //  var id = parseInt(req.params.id); //parseInt khi so sanh id voi so
+       var id = req.params.id;
+      
+   //   console.log(typeof id);
+      var user = db.get('users').find({ id: id}).value();
+      
+      console.log(user);
+      res.render('users/view',{
+          user: user
+      });
+  };
+
+  module.exports.postCreate = function(req, res){
+    console.log(req.body);
+    req.body.id = shortid.generate();
+    db.get('users')//Khong can .value() khi post
+    .push(req.body)
+    .write()
+    
+    //create xong thi chuyen huong sang trang Users
+    res.redirect('/users');
+   };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,55 +1,16 @@
 var express = require('express');
+var controller = require('../controllers/user.controller');
+
 var router = express.Router();
-var db = require('../db');
-var shortid = require('shortid');
 
+router.get('/',controller.index );
 
-router.get('/', function(req, res){
-    res.render('users/index',{
-        users: db.get('users').value()
-    });
-})
+router.get('/search',controller.search);
 
-router.get('/search', function(req, res){
-    var q = req.query.q;
+router.get('/create', controller.create);
 
-    var matchUsers = db.get('users').value().filter(function(user){
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;    
-    });
+router.get('/:id', controller.get);
 
-    res.render('users/index',{
-        users: matchUsers
-    });
-});
-
-
-router.get('/create', function(req, res){
-    res.render('users/create');
-});
-
-router.get('/:id', function(req, res){
-  //  var id = parseInt(req.params.id); //parseInt khi so sanh id voi so
-     var id = req.params.id;
-    
- //   console.log(typeof id);
-    var user = db.get('users').find({ id: id}).value();
-    
-    console.log(user);
-    res.render('users/view',{
-        user: user
-    })
-})
-
-
-router.post('/create', function(req, res){
-    console.log(req.body);
-    req.body.id = shortid.generate();
-    db.get('users')//Khong can .value() khi post
-    .push(req.body)
-    .write()
-    
-    //create xong thi chuyen huong sang trang Users
-    res.redirect('/users');
-});
+router.post('/create', controller.postCreate);
 
 module.exports = router;
